3237012ef83d7790ca098f541d6749f9566a798f
rodrigo-martins-multiedro <rodrigo.martins@multiedro.com.br>
2023-01-04 13:37:20 -0300

    first commit

--- main.py
+++ main.py
@@ -22,5 +22,4 @@ def hello():
     return response.text
 
 if __name__ == "__main__":
-    port = int(os.environ.get("PORT", 8080))
-    app.run(host='127.0.0.1', port=port, debug=True, threaded=True)
+    app.run(host='0.0.0.0')
