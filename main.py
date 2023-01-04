8cce3be1ad4e0243fea7c84ced383b51234fc086
rodrigo-martins-multiedro <rodrigo.martins@multiedro.com.br>
2023-01-04 13:47:50 -0300

    first commit

--- main.py
+++ main.py
@@ -9,17 +9,17 @@ app = Flask(__name__)
 
 @app.route("/",methods=["GET"])
 def hello():
+    try:
+        url = "https://goop.callbox.com.br/webservice/index_get.php"
 
-    url = "https://goop.callbox.com.br/webservice/index_get.php"
+        querystring = {"pKey":"5cd2b49345321019804eac5b4501aa72"," Retorno":"1","calldate":"2023-01-03","calldate[start]":"2023-01-03 00:00:00","calldate[end]":"2023-01-03 23:59:00"," comando":"verificaCDR"}
+        payload = ""
+        headers = {"cookie": "CBXSESSIDWEB=cnc9skp5o4int4u22vnnoa0vv7"}
+        response = requests.request("GET", url, data=payload, headers=headers, params=querystring, timeout=None)
 
-    querystring = {"pKey":"5cd2b49345321019804eac5b4501aa72"," Retorno":"1","calldate":"2023-01-03","calldate[start]":"2023-01-03 00:00:00","calldate[end]":"2023-01-03 23:59:00"," comando":"verificaCDR"}
-    payload = ""
-    headers = {"cookie": "CBXSESSIDWEB=cnc9skp5o4int4u22vnnoa0vv7"}
-    response = requests.request("GET", url, data=payload, headers=headers, params=querystring, timeout=None)
-
-     
-
-    return response.text
+        return response.text
+    except Exception as error:
+        return "Infelizmente ocorreu um erro durante o processamento :"+str(error)
 
 if __name__ == "__main__":
     app.run(host='0.0.0.0')
